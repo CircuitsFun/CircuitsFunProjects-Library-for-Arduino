842fb7451f25bb102e1637a7bc05d350d494e0c2
CircuitsFun <62040230+CircuitsFun@users.noreply.github.com>
2020-05-26 12:44:35 +1000

    Update CircuitsFunProjects.h

--- src/CircuitsFunProjects.h
+++ src/CircuitsFunProjects.h
@@ -2,6 +2,8 @@
 #ifndef CircuitsFunProjects_h
 #define CircuitsFunProjects_h
 
+#include <Arduino.h>
+#include <SPI.h>
 #include <inttypes.h>
 #include "Print.h" 
 #include <Wire.h>
@@ -28,6 +30,44 @@ class Button {
 		long debounceTime;
 		bool checkPress(int timeout, bool requiredState);
 };
+
+class LSM6DS3Class {
+  public:
+    LSM6DS3Class(TwoWire& wire, uint8_t slaveAddress);
+    LSM6DS3Class(SPIClass& spi, int csPin, int irqPin);
+    virtual ~LSM6DS3Class();
+
+    int begin();
+    void end();
+
+    // Accelerometer
+    virtual int readAcceleration(float& x, float& y, float& z); // Results are in G (earth gravity).
+    virtual float accelerationSampleRate(); // Sampling rate of the sensor.
+    virtual int accelerationAvailable(); // Check for available data from accerometer
+
+    // Gyroscope
+    virtual int readGyroscope(float& x, float& y, float& z); // Results are in degrees/second.
+    virtual float gyroscopeSampleRate(); // Sampling rate of the sensor.
+    virtual int gyroscopeAvailable(); // Check for available data from gyroscopeAvailable
+
+
+  private:
+    int readRegister(uint8_t address);
+    int readRegisters(uint8_t address, uint8_t* data, size_t length);
+    int writeRegister(uint8_t address, uint8_t value);
+
+
+  private:
+    TwoWire* _wire;
+    SPIClass* _spi;
+    uint8_t _slaveAddress;
+    int _csPin;
+    int _irqPin;
+
+    SPISettings _spiSettings;
+};
+
+extern LSM6DS3Class IMU;
 
 
 #endif
